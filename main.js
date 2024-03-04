6ca6f27e3bf75d419bb69b45b292e62feb6be636
Lee Ryan <ryanlee@deakin.edu.au>
2024-03-04 11:39:04 +0800

    added comments for task2p assessment

--- main.js
+++ main.js
@@ -1,13 +1,22 @@
-const express = require('express');
-const app = express();
-let port = 3000,
-    server = require('http').createServer(app);
+// import node modules express and http, and initialise expressjs and http server
+const express = require('express'),
+    app = express(),
+    port = 3000,
+    server = require('http').createServer(app),
+    settings = {};
 
+// configure route for path / to accept GET requests
 app.get('/', function (req, res) {
-  console.log(`[server] ${req.ip} ${req.method} ${req.originalUrl}`);
-  res.send('Hello World -- from Lee [ryanlee@deakin.edu.au]');
+  // trigger response to client with a heading HTML element
+  res.send('<h1>Hello World</h1><p>[ryanlee@deakin.edu.au]</p>');
+  // log server, client and resource information to console 
+  console.log(`[${settings.serverInfo}] from ${req.ip} '${req.method} ${req.originalUrl}'`);
 })
 
+// start http server on port 3000
 server.listen(port, () => {
-  console.log(`[server] listening on http://localhost:${server.address().port}`);
+  // set serverInfo for logging
+  settings.serverInfo = `ipaddr=${server.address().address} port=${server.address().port}`;
+  // log testing URL to console
+  console.log(`[${settings.serverInfo}] started: http://localhost:${server.address().port}`);
 });
