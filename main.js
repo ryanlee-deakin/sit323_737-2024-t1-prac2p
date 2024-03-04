d04cbf88e99a13de84ccb52a4f056bf915b16ae1
Lee Ryan <ryanlee@deakin.edu.au>
2024-03-04 11:14:50 +0800

    Added logging and an http server

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const app = express();
+let port = 3000,
+    server = require('http').createServer(app);
 
 app.get('/', function (req, res) {
+  console.log(`[server] ${req.method} ${req.originalUrl}`);
   res.send('Hello World -- from Lee [ryanlee@deakin.edu.au]');
 })
 
-app.listen(3000);
+server.listen(port, () => {
+  console.log(`[server] listening on http://localhost:${server.address().port}`);
+});
